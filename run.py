919a340152868b3024bb94547339b2695fdaf296
ym-pett <ympettinato@gmail.com>
2025-08-11 15:08:28 +0100

    comments for next steps

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from narwhals_daft.dataframe import DaftLazyFrame
 df_native = daft.from_pydict({"a": [1, 2, 3], "b": [4, 5, 6]})
 df_compliant = DaftLazyFrame(df_native, version=Version.MAIN)
 
+# TODO we should't have to do the step above! how can we plug into nw without that extra step? 
 df = nw.from_native(df_compliant)
 result = df.select("a", nw.col("b") * nw.col("a"))
 print(result.collect())
