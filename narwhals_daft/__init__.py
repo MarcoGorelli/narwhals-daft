97317bb24dae86edb0a03daa416fbc0ed48754c6
ym-pett <ympettinato@gmail.com>
2025-08-18 16:37:32 +0100

    nw_daft from_native

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -1,3 +1,9 @@
-from . import dataframe
-from . import expr
+from narwhals_daft import dataframe
+from narwhals_daft import expr as expr
+import daft
+from narwhals.utils import Version
 
+def from_native(native_object: daft.DataFrame, eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
+    if eager_only or series_only:
+        raise ValueError("eager_only and series_only options are not supported as daft is lazy-only.")
+    return dataframe.DaftLazyFrame(native_object, version=Version.MAIN)
