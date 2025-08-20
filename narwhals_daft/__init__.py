45e4e48929835a49b87501ddfbbec7be9c5dd9eb
ym-pett <ympettinato@gmail.com>
2025-08-20 14:32:24 +0100

    removing type alias

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -5,14 +5,12 @@ from narwhals.utils import Version
 from typing import Any
 from typing_extensions import TypeIs
 
-_NativeDaft = daft.DataFrame
-
 def from_native(native_object: daft.DataFrame, eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
     if eager_only or series_only:
         raise ValueError("eager_only and series_only options are not supported as daft is lazy-only.")
     return dataframe.DaftLazyFrame(native_object, version=Version.MAIN)
 
-def is_native_object(obj: Any) -> TypeIs[_NativeDaft]:
+def is_native_object(obj: Any) -> TypeIs[daft.DataFrame]:
     return isinstance(obj, daft.DataFrame)
 
 # def is_native_object(obj: Any) -> TypeIs[_NativeDaft]:
