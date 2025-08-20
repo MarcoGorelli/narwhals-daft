86bd9306879fa38fe58c72974c43d77d76ff8995
ym-pett <ympettinato@gmail.com>
2025-08-20 15:31:48 +0100

    cleanup

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -12,20 +12,3 @@ def from_native(native_object: daft.DataFrame, eager_only: bool, series_only: bo
 
 def is_native_object(obj: Any) -> TypeIs[daft.DataFrame]:
     return isinstance(obj, daft.DataFrame)
-
-# def is_native_object(obj: Any) -> TypeIs[_NativeDaft]:
-#     return is_daft_frame(obj)
-
-# examples with ibis
-
-# def is_native_ibis(obj: Any) -> TypeIs[_NativeIbis]:
-#     return is_ibis_table(obj)
-
-# def is_ibis_table(df: Any) -> TypeIs[ibis.Table]:
-#     """Check whether `df` is a Ibis Table without importing Ibis.
-
-#     Warning:
-#         This method cannot be called on Narwhals DataFrame/LazyFrame.
-#     """
-#     _warn_if_narwhals_df_or_lf(df)
-#     return (ibis := get_ibis()) is not None and isinstance(df, ibis.expr.types.Table)
