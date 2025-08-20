a16531131279148c63c6a664bb9e0634ee4d95e6
ym-pett <ympettinato@gmail.com>
2025-08-20 14:21:06 +0100

    wip working on types

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -3,16 +3,16 @@ from narwhals_daft import expr as expr
 import daft
 from narwhals.utils import Version
 from typing import Any
-# from typing_extensions import TypeIs
+from typing_extensions import TypeIs
 
-# _NativeDaft: TypeAlias = "daft.DataFrame"
+_NativeDaft = daft.DataFrame
 
 def from_native(native_object: daft.DataFrame, eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
     if eager_only or series_only:
         raise ValueError("eager_only and series_only options are not supported as daft is lazy-only.")
     return dataframe.DaftLazyFrame(native_object, version=Version.MAIN)
 
-def is_native_object(obj: Any) -> Any:
+def is_native_object(obj: Any) -> TypeIs[_NativeDaft]:
     return isinstance(obj, daft.DataFrame)
 
 # def is_native_object(obj: Any) -> TypeIs[_NativeDaft]:
