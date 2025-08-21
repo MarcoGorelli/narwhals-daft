55b9018a9289f902b866e84ee10b82559f812e33
ym-pett <ympettinato@gmail.com>
2025-08-21 14:53:44 +0100

    version with from future..

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import sys
 from narwhals_daft import dataframe
 from narwhals_daft import expr as expr
@@ -7,12 +8,12 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import daft
 
-def from_native(native_object: "daft.DataFrame", eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
+def from_native(native_object: daft.DataFrame, eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
     if eager_only or series_only:
         raise ValueError("eager_only and series_only options are not supported as daft is lazy-only.")
     return dataframe.DaftLazyFrame(native_object, version=Version.MAIN)
 
-def is_native_object(native_object:  "daft.DataFrame") -> bool:
+def is_native_object(native_object:  daft.DataFrame) -> bool:
     if (daft := sys.modules.get('daft', None)) is not None:
         return isinstance(native_object, daft.DataFrame)
     return False
