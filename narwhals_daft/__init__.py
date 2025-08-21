eda31ce8766f6687db0ab6ad5a532829d702c490
ym-pett <ympettinato@gmail.com>
2025-08-21 14:22:45 +0100

    cleanup

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -2,8 +2,7 @@ import sys
 from narwhals_daft import dataframe
 from narwhals_daft import expr as expr
 from narwhals.utils import Version
-from typing import Any, TYPE_CHECKING
-from typing_extensions import TypeIs
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import daft
@@ -17,11 +16,3 @@ def is_native_object(native_object:  "daft.DataFrame") -> bool:
     if (daft := sys.modules.get('daft', None)) is not None:
         return isinstance(native_object, daft.DataFrame)
     return False
-
-# # longer form for personal understanding:
-# def is_native_object(native_object:  "daft.DataFrame") -> bool:
-#     daft = sys.modules.get("daft")
-#     if daft is not None:
-#         return isinstance(native_object, daft.DataFrame)
-#     return False
-
