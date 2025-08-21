b5332290e0351cef2d8eddbbefd4701409ab1bd2
ym-pett <ympettinato@gmail.com>
2025-08-21 12:53:09 +0100

    wip Marco's suggestions

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -1,14 +1,27 @@
+import sys
 from narwhals_daft import dataframe
 from narwhals_daft import expr as expr
-import daft
 from narwhals.utils import Version
-from typing import Any
+from typing import Any, TYPE_CHECKING
 from typing_extensions import TypeIs
 
-def from_native(native_object: daft.DataFrame, eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
+if TYPE_CHECKING:
+    import daft
+
+def from_native(native_object: "daft.DataFrame", eager_only: bool, series_only: bool) -> dataframe.DaftLazyFrame:
     if eager_only or series_only:
         raise ValueError("eager_only and series_only options are not supported as daft is lazy-only.")
     return dataframe.DaftLazyFrame(native_object, version=Version.MAIN)
 
-def is_native_object(obj: Any) -> TypeIs[daft.DataFrame]:
-    return isinstance(obj, daft.DataFrame)
+def is_native_object(native_object:  "daft.DataFrame") -> bool:
+    if (daft := sys.modules.get('daft', None)) is not None:
+        return isinstance(native_object, daft.DataFrame)
+    return False
+
+# # longer form for personal understanding:
+# def is_native_object(native_object:  "daft.DataFrame") -> bool:
+#     daft = sys.modules.get("daft")
+#     if daft is not None:
+#         return isinstance(native_object, daft.DataFrame)
+#     return False
+
