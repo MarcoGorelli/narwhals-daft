479b6ca128ccafee35acb3381bca64c0c2d9df84
ym-pett <ympettinato@gmail.com>
2025-09-08 11:37:37 +0100

    wip, trying

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 import sys
 from narwhals_daft import dataframe
 from narwhals_daft import expr as expr
+from narwhals_daft.namespace import DaftNamespace
 from narwhals.utils import Version
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-if TYPE_CHECKING:
-    import daft
+# if TYPE_CHECKING:
+#     import daft
 
-def from_native(native_object: daft.DataFrame, version: Version) -> dataframe.DaftLazyFrame:
-    return dataframe.DaftLazyFrame(native_object, version=version)
+def __narwhals_namespace__(unclear:Any) -> DaftNamespace:
+    return DaftNamespace.from_native(native_object)
 
 def is_native_object(native_object:  daft.DataFrame) -> bool:
     if (daft := sys.modules.get('daft', None)) is not None:
