5e84b2721a4f8e45fdf2b84ac2cc9142aba6b267
ym-pett <ympettinato@gmail.com>
2025-09-08 13:39:05 +0100

    revert to commit 479b6ca128ccafee35acb3381bca64c0c2d9df84

--- narwhals_daft/namespace.py
+++ narwhals_daft/namespace.py
@@ -16,6 +16,7 @@ from narwhals_daft.utils import lit, narwhals_to_native_dtype
 from narwhals._utils import Implementation, not_implemented
 
 if TYPE_CHECKING:
+    import daft
     from collections.abc import Iterable
     from narwhals._utils import Version
     from narwhals.dtypes import DType
@@ -25,14 +26,11 @@ if TYPE_CHECKING:
 class DaftNamespace(LazyNamespace[DaftLazyFrame, DaftExpr, daft.DataFrame]):
     _implementation: Implementation = Implementation.UNKNOWN
 
-    def from_native(self, data: daft.DataFrame | Any, **kwargs: Any) -> DaftLazyFrame:
-        if kwargs:
-            msg = "eager_only and series_only options are not supported as daft is lazy-only."
-            raise ValueError(msg)
-        return super().from_native(data)
-
     def __init__(self, *, version: Version) -> None:
         self._version = version
+
+    def from_native(self, native_object: daft.DataFrame, version: Version) -> DaftLazyFrame:
+        return DaftLazyFrame(native_object, version=version)
 
     @property
     def _expr(self) -> type[DaftExpr]:
