f028d3e28f915587f68d914ec8d82e5663d5a864
ym-pett <ympettinato@gmail.com>
2025-09-09 10:45:06 +0100

    fixed import

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -5,8 +5,6 @@ from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from narwhals_daft.namespace import DaftNamespace
-    # TODO discuss: is daft import needed here? see below
-    import daft
 
 def __narwhals_namespace__(version: Version) -> DaftNamespace:
     from narwhals_daft.namespace import DaftNamespace
@@ -14,6 +12,5 @@ def __narwhals_namespace__(version: Version) -> DaftNamespace:
 
 def is_native_object(native_object:  Any) -> bool:
     if (daft := sys.modules.get('daft', None)) is not None:
-        # TODO discuss: could we import daft here, only if it's in modules? 
         return isinstance(native_object, daft.DataFrame)
     return False
