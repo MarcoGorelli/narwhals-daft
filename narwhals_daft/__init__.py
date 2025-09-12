e5a791714e229dff68802520e94550be9ecdcbc7
ym-pett <ympettinato@gmail.com>
2025-09-12 13:54:08 +0100

    added missing from_native

--- narwhals_daft/__init__.py
+++ narwhals_daft/__init__.py
@@ -4,14 +4,20 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from narwhals_daft.namespace import DaftNamespace
+    from narwhals_daft.dataframe import DaftLazyFrame
 
     from narwhals.utils import Version
+    from typing_extensions import TypeIs
 
 
 def __narwhals_namespace__(version: Version) -> DaftNamespace:  # noqa: N807
     from narwhals_daft.namespace import DaftNamespace
 
     return DaftNamespace(version=version)
+
+def is_native(native_object:object) -> TypeIs[DaftLazyFrame]:
+    import daft
+    return isinstance(native_object, daft.DataFrame)
 
 
 NATIVE_PACKAGE = "daft"
