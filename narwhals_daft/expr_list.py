bf565be1e191bdbde42af0b752a575c5f7abe64a
ym-pett <ympettinato@gmail.com>
2025-12-15 11:03:41 +0100

    wip expr_list

---
+++ narwhals_daft/expr_list.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from narwhals._compliant import LazyExprNamespace
+from narwhals._compliant.any_namespace import ListNamespace
+from narwhals._utils import not_implemented
