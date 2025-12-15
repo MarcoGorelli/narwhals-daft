b1fd5043009a5518601996a107be280acd804735
ym-pett <ympettinato@gmail.com>
2025-12-15 12:24:16 +0100

    fixing daftexpr import

--- narwhals_daft/expr_list.py
+++ narwhals_daft/expr_list.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from narwhals._compliant import LazyExprNamespace
 from narwhals._compliant.any_namespace import ListNamespace
 from narwhals._utils import not_implemented
 
+if TYPE_CHECKING:
+
+    from narwhals_daft.expr import DaftExpr
+
 
 class ExprListNamespace(LazyExprNamespace["DaftExpr"], ListNamespace["DaftExpr"]):
+
     len = not_implemented()
     unique = not_implemented()
     contains = not_implemented()
