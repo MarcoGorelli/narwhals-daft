e9ffe5ae7e5e532a9454a506fd6b78d51be5b31a
ym-pett <ympettinato@gmail.com>
2025-12-15 12:09:05 +0100

    skeleton for exprlistnamespace and updated run_tests

--- narwhals_daft/expr_list.py
+++ narwhals_daft/expr_list.py
@@ -1,7 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from narwhals._compliant import LazyExprNamespace
 from narwhals._compliant.any_namespace import ListNamespace
 from narwhals._utils import not_implemented
+
+
+class ExprListNamespace(LazyExprNamespace["DaftExpr"], ListNamespace["DaftExpr"]):
+    len = not_implemented()
+    unique = not_implemented()
+    contains = not_implemented()
+    get = not_implemented()
+    min = not_implemented()
+    max = not_implemented()
+    mean = not_implemented()
+    median = not_implemented()
+    sum = not_implemented()
